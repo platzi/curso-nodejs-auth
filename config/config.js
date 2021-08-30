fc5a9e4250b77673db51da6e83e48618db463425
Nicolas Molina Monroy <hi@nicobytes.com>
2021-08-30 14:37:09 -0400

    :construction: listen port

--- config/config.js
+++ config/config.js
@@ -4,11 +4,6 @@ const config = {
   env: process.env.NODE_ENV || 'dev',
   isProd: process.env.NODE_ENV === 'production',
   port: process.env.PORT || 3000,
-  dbUser:  process.env.DB_USER,
-  dbPassword:  process.env.DB_PASSWORD,
-  dbHost:  process.env.DB_HOST,
-  dbName:  process.env.DB_NAME,
-  dbPort:  process.env.DB_PORT,
   dbUrl: process.env.DATABASE_URL,
 }
 
--- index.js
+++ index.js
@@ -38,5 +38,5 @@ app.use(errorHandler);
 
 
 app.listen(port, () => {
-  console.log('Mi port' +  port);
+  console.log(`Mi port ${port}`);
 });
