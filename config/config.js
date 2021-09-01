64ce9fff25a2dfc2d55a424af0206582e19bde05
Nicolas Molina Monroy <hi@nicobytes.com>
2021-09-01 06:25:21 -0400

    :construction: token in service

--- config/config.js
+++ config/config.js
@@ -6,6 +6,7 @@ const config = {
   port: process.env.PORT || 3000,
   dbUrl: process.env.DATABASE_URL,
   apiKey: process.env.API_KEY,
+  jwtSecret: process.env.JWT_SECRET,
 }
 
 module.exports = { config };
--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const { config } = require('./../config/config');
 
 
 const router = express.Router();
@@ -8,7 +11,16 @@ router.post('/login',
   passport.authenticate('local', {session: false}),
   async (req, res, next) => {
     try {
-      res.json(req.user);
+      const user = req.user;
+      const payload = {
+        sub: user.id,
+        role: user.role
+      }
+      const token = jwt.sign(payload, config.jwtSecret);
+      res.json({
+        user,
+        token
+      });
     } catch (error) {
       next(error);
     }
