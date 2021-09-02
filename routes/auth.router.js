f7cd7ab7f9cc45261d23f2734a78f82b78070923
Nicolas Molina Monroy <hi@nicobytes.com>
2021-09-02 11:29:21 -0400

    :construction: work

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -30,4 +30,16 @@ router.post('/recovery',
   }
 );
 
+router.post('/change-password',
+  async (req, res, next) => {
+    try {
+      const { token, newPassword } = req.body;
+      const rta = await service.changePassword(token, newPassword);
+      res.json(rta);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 module.exports = router;
--- services/auth.service.js
+++ services/auth.service.js
@@ -53,6 +53,21 @@ class AuthService {
     return rta;
   }
 
+  async changePassword(token, newPassword) {
+    try {
+      const payload = jwt.verify(token, config.jwtSecret);
+      const user = await service.findOne(payload.sub);
+      if (user.recoveryToken !== token) {
+        throw boom.unauthorized();
+      }
+      const hash = await bcrypt.hash(newPassword, 10);
+      await service.update(user.id, {recoveryToken: null, password: hash});
+      return { message: 'password changed' };
+    } catch (error) {
+      throw boom.unauthorized();
+    }
+  }
+
   async sendMail(infoMail) {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
